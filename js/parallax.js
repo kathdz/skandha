6896dfdd3beeb50aa7a473fbd9884d19a42218ee
Katherine <catherinehernandez@MacBook-Air-de-Catherine.local>
2015-10-15 17:10:06 -0500

    Proyecto final

---
+++ js/parallax.js
@@ -0,0 +1,9 @@
+<script>
+    	$(document).ready(function() {
+		//"use strict";
+        $('#content').parallax("50%", 0.5, true);   		
+        $('#join_skandha').parallax("50%", 0.5, true);
+        $('#footer').parallax("50%", 0.5, true);
+    	});
+
+    </script>
